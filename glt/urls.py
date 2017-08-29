c0c9c48e624e8a3129800dbb227677b115eaee85
Marc Schwartz <mathemaat@hotmail.com>
2017-08-29 16:04:02 +0200

    Initialise organs app

--- glt/urls.py
+++ glt/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^articles/', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^organs/', include('organs.urls')),
 ]
 
