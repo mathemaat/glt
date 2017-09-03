1a67043b89528a2f56c48a386f8517d850ae9393
Marc Schwartz <mathemaat@hotmail.com>
2017-09-03 14:10:31 +0200

    Add comments to urls

--- glt/urls.py
+++ glt/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
+  # homepage
   url(r'^$', TemplateView.as_view(template_name='static/index.html'), name='home'),
+
+  # admin site
   url(r'^admin/', admin.site.urls),
 ]
 
