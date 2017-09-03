3eca94cdac86a503b202046f653a41dc80cacfe9
Marc Schwartz <mathemaat@hotmail.com>
2017-09-03 14:09:07 +0200

    Reduce number of spaces per tab

--- glt/urls.py
+++ glt/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='static/index.html'), name='home'),
-    url(r'^admin/', admin.site.urls),
+  url(r'^$', TemplateView.as_view(template_name='static/index.html'), name='home'),
+  url(r'^admin/', admin.site.urls),
 ]
 
