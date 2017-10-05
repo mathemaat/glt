dc3a83f501ca0974ee9512ba7c4414b1221769ae
Marc Schwartz <mathemaat@hotmail.com>
2017-10-05 22:12:12 +0200

    Implement the first version of the organ system index page

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
+  # organ-systems
+  url(r'^organ-systems/$', views.OrganSystemIndexView.as_view(), name='organ_system_index'),
   # e.g. organs/24/brain
   url(r'^organs/(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.OrganDetailView.as_view(), name='organ_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.views import generic
 
-from models import Organ
+from models import Organ, OrganSystem
+
+class OrganSystemIndexView(generic.ListView):
+  model = OrganSystem
+  template_name = 'articles/organ_system/index.html'
 
 class OrganDetailView(generic.DetailView):
   model = Organ
