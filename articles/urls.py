4eddc9bf4c16b71fbcec22d582c62af8dfe9c315
Marc Schwartz <mathemaat@hotmail.com>
2017-10-08 11:16:51 +0200

    Implement the first version of the topic index page

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,8 @@ app_name = 'articles'
 urlpatterns = [
   # organ-systems
   url(r'^organ-systems/$', views.OrganSystemIndexView.as_view(), name='organ_system_index'),
+  # topics
+  url(r'^topics/$', views.TopicIndexView.as_view(), name='topic_index'),
   # e.g. organs/24/brain
   url(r'^organs/(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.OrganDetailView.as_view(), name='organ_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -4,11 +4,15 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.views import generic
 
-from models import Organ, OrganSystem
+from models import Organ, OrganSystem, Topic
 
 class OrganSystemIndexView(generic.ListView):
   model = OrganSystem
   template_name = 'articles/organ_system/index.html'
+
+class TopicIndexView(generic.ListView):
+  model = Topic
+  template_name = 'articles/topic/index.html'
 
 class OrganDetailView(generic.DetailView):
   model = Organ
