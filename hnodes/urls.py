d27dfcb1521de92f11330c99cadfdf559763e9e4
Marc Schwartz <mathemaat@hotmail.com>
2017-11-11 16:35:00 +0100

    Add options to add siblings to the left or right

--- hnodes/urls.py
+++ hnodes/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
   url(r'^(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.HNodeDetailView.as_view(), name='detail'),
   # e.g. 15/tissue/insert-child
   url(r'^(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/insert-child/$', views.insertChild, name='insert-child'),
+  # e.g. 15/tissue/insert-sibling-left
+  url(r'^(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/insert-sibling-left/$',  views.insertSiblingLeft,  name='insert-sibling-left'),
+  # e.g. 15/tissue/insert-sibling-right
+  url(r'^(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/insert-sibling-right/$', views.insertSiblingRight, name='insert-sibling-right'),
   # e.g. 15/tissue/delete
   url(r'^(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/delete/$', views.delete, name='delete'),
   # e.g. node-tree
--- hnodes/views.py
+++ hnodes/views.py
@@ -39,6 +39,50 @@ def insertChild(request, pk, slug):
 
   return HttpResponseRedirect(reverse('hnodes:detail', args=(new.id, 'todo')))
 
+def insertSiblingLeft(request, pk, slug):
+  hnode = get_object_or_404(HNode, pk=pk)
+
+  with connection.cursor() as cursor:
+    query = ('UPDATE hnodes_hnode '
+             'SET vleft = vleft + 2 '
+             'WHERE vleft >= %(vleft)s')
+    cursor.execute(query, {'vleft': hnode.vleft})
+
+    query = ('UPDATE hnodes_hnode '
+             'SET vright = vright + 2 '
+             'WHERE vright >= %(vleft)s')
+    cursor.execute(query, {'vleft': hnode.vleft})
+
+  new = HNode()
+  new.description = request.POST['description']
+  new.vleft = hnode.vleft
+  new.vright = hnode.vleft + 1
+  new.save();
+
+  return HttpResponseRedirect(reverse('hnodes:detail', args=(new.id, 'todo')))
+
+def insertSiblingRight(request, pk, slug):
+  hnode = get_object_or_404(HNode, pk=pk)
+
+  with connection.cursor() as cursor:
+    query = ('UPDATE hnodes_hnode '
+             'SET vleft = vleft + 2 '
+             'WHERE vleft > %(vright)s')
+    cursor.execute(query, {'vright': hnode.vright})
+
+    query = ('UPDATE hnodes_hnode '
+             'SET vright = vright + 2 '
+             'WHERE vright > %(vright)s')
+    cursor.execute(query, {'vright': hnode.vright})
+
+  new = HNode()
+  new.description = request.POST['description']
+  new.vleft = hnode.vright + 1
+  new.vright = hnode.vright + 2
+  new.save();
+
+  return HttpResponseRedirect(reverse('hnodes:detail', args=(new.id, 'todo')))
+
 def delete(request, pk, slug):
   hnode = get_object_or_404(HNode, pk=pk)
 
