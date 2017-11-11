efa7f1584e7447ce125ce36c1371c4a65eea5dde
Marc Schwartz <mathemaat@hotmail.com>
2017-11-11 18:32:11 +0100

    WIP

--- glt/static/js/application.js
+++ glt/static/js/application.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+  console.log('?');
+  $("form.insert-sibling-node").each(function(){
+    var form = $(this);
+    $(this).submit(function(e){
+      e.preventDefault();
+      alert(form.tagName);
+/*
+      var input = $("input[name='description']", form);
+      alert('3:' + input.tagName);
+      alert('Length: ' + input.value().length());
+      return false;
+      if (input.value().length() == 0)
+        alert('Boo!');
+*/
+    });
+  });
 });
 
